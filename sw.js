f0185daa73a4d4f13724879b3d702e05c054aaa0
LasagnaMan13 <eschaller517@gmail.com>
2025-07-14 13:34:23 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/workout_tracker_advanced.html',
+        '/manifest.json',
+        '/icon.png',
+        'https://cdn.jsdelivr.net/npm/chart.js',
+        'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns',
+        'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js',
+        'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
